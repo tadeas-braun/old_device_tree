ea3e47e8379ecfce7011d003cac8b2defd42dffb
munjeni <sharecard17@gmail.com>
2013-09-20 19:35:38 +0200

    CM10.2 have new bootanimation zip names

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST27i_1262-1120 BUILD_FINGERPRINT=SEMC/ST27i_1262-1120/ST27i:4.0.4/6.1.1.B.1.10/9bt3zw:user/release-keys PRIVATE_BUILD_DESC="ST27i-user 4.0.4 6.1.1.B.1.10 9bt3zw test-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-320x480
+TARGET_BOOTANIMATION_NAME := 320
 
 PRODUCT_NAME := cm_lotus
 PRODUCT_DEVICE := lotus
