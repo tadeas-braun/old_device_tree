1b364ab9234a49455f827dbe1ff649ae60875c15
munjeni <sharecard17@gmail.com>
2013-09-22 10:36:04 +0200

    fix multiple sdcards mounts in recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,7 @@ BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/sony/lotus/recovery/recovery-ke
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_10x18.h\"
 
 BOARD_UMS_LUNFILE := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun0/file"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun0/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun%d/file"
 
 # Kernel information
 TARGET_KERNEL_CONFIG := xperiago_defconfig
