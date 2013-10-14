9b5bd1bee68c05a31db10cc8991a1dba2c754978
munjeni <sharecard17@gmail.com>
2013-10-14 17:32:43 +0200

    hack: fixup recovery usb and reinit adbd after moving menu

--- recovery/recovery-keys.c
+++ recovery/recovery-keys.c
@@ -28,6 +28,26 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
 
 int device_handle_key(int key_code, int visible) {
 
+    extern int __system(const char *command);
+    static int fixup_xperia_usb = 0;
+
+    if (!fixup_xperia_usb) {
+        __system("/sbin/cat /sys/devices/platform/ab8500-i2c.0/ab8500-usb.0/boot_time_device > /sys/devices/platform/ab8500-i2c.0/ab8500-usb.0/boot_time_device");
+        __system("/sbin/killall -9 adbd");
+        __system("/sbin/echo 0 >/sys/class/android_usb/android0/enable");
+        __system("/sbin/echo 0FCE >/sys/class/android_usb/android0/idVendor");
+        __system("/sbin/echo 617E >/sys/class/android_usb/android0/idProduct");
+        __system("/sbin/echo 'mass_storage,adb' >/sys/class/android_usb/android0/functions");
+        __system("/sbin/echo 1 >/sys/class/android_usb/android0/enable");
+        __system("/sbin/setprop sys.usb.state mass_storage,adb");
+        __system("/sbin/setprop sys.usb.config mass_storage,adb");
+        __system("/sbin/setprop persist.sys.usb.config mass_storage,adb");
+        __system("/sbin/start adbd");
+        __system("/sbin/setprop ro.build.product lotus");
+
+        fixup_xperia_usb = 1;
+    }
+
     if (visible) {
         switch (key_code) {
             case KEY_CAPSLOCK:
