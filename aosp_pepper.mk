33a241b88f1aae27d5e20cf08b4556d3e429b1d4
tadeas482 <tadeas.braun@gmail.com>
2013-11-01 23:18:20 +0100

    Create aosp_pepper.mk

--- aosp_pepper.mk
+++ aosp_pepper.mk
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/sony/lotus/full_lotus.mk)
+$(call inherit-product, device/sony/pepper/full_pepper.mk)
 
-PRODUCT_NAME := aosp_lotus
+PRODUCT_NAME := aosp_pepper
