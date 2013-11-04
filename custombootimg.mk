738e3669fe94a1252cf7c85d8ac5b6d2a22ea498
munjeni <sharecard17@gmail.com>
2013-11-04 16:58:31 +0100

    from some unknown reason for me, this not working under android 4.4

--- custombootimg.mk
+++ custombootimg.mk
@@ -12,7 +12,6 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(recovery_ramdisk) $(INSTA
 	$(hide) mv $(PRODUCT_OUT)/recovery/root/recovery $(PRODUCT_OUT)/recovery/root/sbin/
 	$(hide) cp -R $(PRODUCT_OUT)/recovery/root/etc $(PRODUCT_OUT)/combinedroot/
 	$(hide) cp -R $(PRODUCT_OUT)/recovery/root/tmp $(PRODUCT_OUT)/combinedroot/
-	$(hide) cp -R $(PRODUCT_OUT)/../../../../device/sony/$(TARGET_DEVICE)/logcat_grab/logcat_grab $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp -R $(PRODUCT_OUT)/../../../../device/sony/$(TARGET_DEVICE)/prebuilt/root/default.prop $(PRODUCT_OUT)/combinedroot/
 	$(hide) cp -R $(PRODUCT_OUT)/../../../../device/sony/$(TARGET_DEVICE)/recovery/recovery_ramdisk.gz $(PRODUCT_OUT)/combinedroot/sbin/recovery_ramdisk.gz
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot > $(PRODUCT_OUT)/combinedroot.cpio
