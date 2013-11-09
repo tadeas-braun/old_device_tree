dfd463d9627314b457d8d0d52c5a4338ee718422
tadeas482 <tadeas.braun@gmail.com>
2013-11-09 12:24:03 +0100

    Create cm.mk

---
+++ cm.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/sony/pepper/full_pepper.mk)
+
+# Inherit CM common GSM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := cm_pepper
+PRODUCT_DEVICE := pepper
