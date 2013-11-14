b1cd5f97e8d9ccdec6363aa4f6ceb54bb5c72273
tadeas482 <tadeas.braun@gmail.com>
2013-11-14 22:13:19 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 $(call inherit-product, device/sony/pepper/full_pepper.mk)
-=======
-$(call inherit-product, device/sony/lotus/full_lotus.mk)
->>>>>>> upstream/cm-11.0
 
 # Inherit CM common GSM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
