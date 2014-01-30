7e5116960213385367fbfa78897b2195f523b2ab
munjeni <sharecard17@gmail.com>
2014-01-30 19:58:05 +0100

    ops

--- include/device_perms.h
+++ include/device_perms.h
@@ -1,6 +1,6 @@
 /* needed for bluetoothd (Bluez 5.12) */
 #define CONTROL_PERMS_APPEND \
-    { "bluetoothd", AID_BLUETOOTH, AID_BLUETOOTH },
+    { "bluetoothd", AID_BLUETOOTH, AID_BLUETOOTH }, \
     { "bluetoothd-snoop", AID_NOBODY, AID_NOBODY },
 
 /* need for camera */
